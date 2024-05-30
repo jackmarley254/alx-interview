b4b8a2333d00784a63c3ba983534a40b525078f1
jackmarley254 <engndiritu1@gmail.com>
2024-05-30 16:49:48 +0300

    Updating N queens

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -2,13 +2,15 @@
 
 import sys
 
-def is_valid(board, row, col):
+
+def is_valid(board, row, column):
     for i in range(row):
-        if board[i] == col or \
-            board[i] - i == col - row or \
-            board[i] + i == col + row:
+        if board[i] == column or \
+           board[i] - i == column - row or \
+           board[i] + i == column + row:
             return False
     return True
+
 
 def solve_nqueens(N):
     def place_queens(row, board):
@@ -20,31 +22,33 @@ def solve_nqueens(N):
                 board[row] = col
                 place_queens(row + 1, board)
                 board[row] = -1
-                
+
     solutions = []
     board = [-1] * N
     place_queens(0, board)
     return solutions
 
+
 def print_solutions(solutions):
     for solution in solutions:
         formatted_solution = [[i, solution[i]] for i in range(len(solution))]
         print(formatted_solution)
-        
+
+
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
-        
+
     try:
         N = int(sys.argv[1])
     except ValueError:
         print("N must be a number")
         sys.exit(1)
-        
+
     if N < 4:
         print("N must be at least 4")
         sys.exit(1)
-        
+
     solutions = solve_nqueens(N)
-    print_solutions(solutions)                    
+    print_solutions(solutions)
